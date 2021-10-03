72a4961c518761171ae3b61eb9033b7b65e08e16
chakib.bensari <chakib_bensari@hotmail.fr>
2021-10-03 16:35:09 +0200

    resolve .gitignore conflict

---
+++ src/test/java/dz/chicov/graphql/GraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.chicov.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
